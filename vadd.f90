9da1d9f8e71aeff47a1fa48884c2a26a44ea80ad
Tom Deakin <tom.deakin@bristol.ac.uk>
2018-02-18 21:53:30 +0000

    Add Vector Add examples

---
+++ vadd.f90
@@ -0,0 +1,48 @@
+
+! Vector addition
+program vadd
+
+  use timer
+
+  implicit none
+
+  integer :: N=50000000
+  real(kind=8), allocatable :: A(:), B(:), C(:)
+  integer :: i
+  real(kind=8) :: start, end
+
+  ! Allocate memory
+  allocate(A(N))
+  allocate(B(N))
+  allocate(C(N))
+
+  ! Initilise data
+  do i = 1, N
+    A(i) = 1.0_8
+    B(i) = 2.0_8
+    C(i) = 0.0_8
+  end do
+
+  ! Start timer
+  call wtime(start)
+
+  ! Vector addition
+  do i = 1, N
+    C(i) = A(i) + B(i)
+  end do
+
+  ! Stop timer
+  call wtime(end)
+
+  ! Print result
+  write(*,"(A)")         "------------------------------------"
+  write(*,"(A,F10.3)")   "runtime:  ", end-start
+  if (any(C .ne. 3.0_8)) then
+    write(*,"(A)")       "WARNING: results incorrect"
+  end if
+  write(*,"(A)")         "------------------------------------"
+
+  ! Free memory
+  deallocate(A,B)
+
+end program vadd
